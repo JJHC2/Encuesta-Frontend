64d2f5b615b3401771e3409a5ff5ae1e394506f3
Jair Campos <al222110834@utvtol.edu.mx>
2024-11-28 21:33:08 -0600

    Cambios final diseño encuesta

--- src/sections/Trabajo/SeccionTrabajo.jsx
+++ src/sections/Trabajo/SeccionTrabajo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Paper } from "@mui/material";
+import { Box, Typography, Paper, Grid } from "@mui/material";
 import PreguntaRadioGroup from "../../components/Encuesta/PreguntaRadioGroup";
 
 const SeccionTrabajo = ({ formData, HandleInputChange }) => {
@@ -13,56 +13,78 @@ const SeccionTrabajo = ({ formData, HandleInputChange }) => {
           Trayectoria Profesional
         </Typography>
 
-        <PreguntaRadioGroup
-          pregunta="13.- Trabajas Actualmente?"
-          opciones={["Si", "No"]}
-          seleccion={formData.trabajas}
-          onChange={(respuesta) => HandleInputChange("trabajas", respuesta)}
-        />
+        
+        <Grid container spacing={3} direction="row">
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="13.- Trabajas Actualmente?"
+              opciones={["Si", "No"]}
+              seleccion={formData.trabajas}
+              onChange={(respuesta) => HandleInputChange("trabajas", respuesta)}
+            />
+          </Grid>
 
-        <Typography variant="h6" gutterBottom>
-          14. Califica del 1 (nada importante o no influyente) al 5 (muy importante o muy influyente) las razones o los factores que hicieron que te contratarán: (excepto becarios, autónomos por cuenta propia y dueños de negocio propio o familiar)
-        </Typography>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" gutterBottom>
+              14. Califica del 1 (nada importante o no influyente) al 5 (muy importante o muy influyente) las razones o los factores que hicieron que te contratarán: (excepto becarios, autónomos por cuenta propia y dueños de negocio propio o familiar)
+            </Typography>
+          </Grid>
 
-       
-        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, marginBottom: 2 }}>
-          <PreguntaRadioGroup
-            pregunta="Los conocimientos teóricos"
-            opciones={["1", "2", "3", "4", "5"]}
-            seleccion={formData.conocimientosteoricos}
-            onChange={(respuesta) => HandleInputChange("conocimientosteoricos", respuesta)}
-          />
-          <PreguntaRadioGroup
-            pregunta="Los conocimientos prácticos"
-            opciones={["1", "2", "3", "4", "5"]}
-            seleccion={formData.conocimientospracticos}
-            onChange={(respuesta) => HandleInputChange("conocimientospracticos", respuesta)}
-          />
-          <PreguntaRadioGroup
-            pregunta="Dominio del inglés"
-            opciones={["1", "2", "3", "4", "5"]}
-            seleccion={formData.dominioingles}
-            onChange={(respuesta) => HandleInputChange("dominioingles", respuesta)}
-          />
-          <PreguntaRadioGroup
-            pregunta="La formación en el uso de la informática y de las nuevas tecnologías"
-            opciones={["1", "2", "3", "4", "5"]}
-            seleccion={formData.formacioninformatica}
-            onChange={(respuesta) => HandleInputChange("formacioninformatica", respuesta)}
-          />
-          <PreguntaRadioGroup
-            pregunta="La manera de ser: personalidad, habilidades sociales, comunicación..."
-            opciones={["1", "2", "3", "4", "5"]}
-            seleccion={formData.personalidad}
-            onChange={(respuesta) => HandleInputChange("personalidad", respuesta)}
-          />
-          <PreguntaRadioGroup
-            pregunta="La capacidad de gestión y planificación"
-            opciones={["1", "2", "3", "4", "5"]}
-            seleccion={formData.capacidadgestion}
-            onChange={(respuesta) => HandleInputChange("capacidadgestion", respuesta)}
-          />
-        </Box>
+          {/* Las siguientes preguntas estarán en una fila horizontal */}
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="Los conocimientos teóricos"
+              opciones={["1", "2", "3", "4", "5"]}
+              seleccion={formData.conocimientosteoricos}
+              onChange={(respuesta) => HandleInputChange("conocimientosteoricos", respuesta)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="Los conocimientos prácticos"
+              opciones={["1", "2", "3", "4", "5"]}
+              seleccion={formData.conocimientospracticos}
+              onChange={(respuesta) => HandleInputChange("conocimientospracticos", respuesta)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="Dominio del inglés"
+              opciones={["1", "2", "3", "4", "5"]}
+              seleccion={formData.dominioingles}
+              onChange={(respuesta) => HandleInputChange("dominioingles", respuesta)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="La formación en el uso de la informática y de las nuevas tecnologías"
+              opciones={["1", "2", "3", "4", "5"]}
+              seleccion={formData.formacioninformatica}
+              onChange={(respuesta) => HandleInputChange("formacioninformatica", respuesta)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="La manera de ser: personalidad, habilidades sociales, comunicación..."
+              opciones={["1", "2", "3", "4", "5"]}
+              seleccion={formData.personalidad}
+              onChange={(respuesta) => HandleInputChange("personalidad", respuesta)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <PreguntaRadioGroup
+              pregunta="La capacidad de gestión y planificación"
+              opciones={["1", "2", "3", "4", "5"]}
+              seleccion={formData.capacidadgestion}
+              onChange={(respuesta) => HandleInputChange("capacidadgestion", respuesta)}
+            />
+          </Grid>
+        </Grid>
       </Paper>
     </Box>
   );
